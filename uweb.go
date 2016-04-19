f0c9974b2c97e1714a1600c827e2d5970fee52f9
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-19 08:35:16 -0300

    .

--- uweb.go
+++ uweb.go
@@ -1,19 +1,20 @@
 package uweb
 
 import (
+	"fmt"
 	"net/http"
 )
 
 const (
-	GET     = "GET"
-	HEAD    = "HEAD"
-	POST    = "POST"
-	PUT     = "PUT"
-	PATCH   = "PATCH"
-	DELETE  = "DELETE"
-	CONNECT = "CONNECT"
-	OPTIONS = "OPTIONS"
-	TRACE   = "TRACE"
+	get     = "GET"
+	head    = "HEAD"
+	post    = "POST"
+	put     = "PUT"
+	patch   = "PATCH"
+	delete  = "DELETE"
+	connect = "CONNECT"
+	options = "OPTIONS"
+	trace   = "TRACE"
 )
 
 type UWeb struct {
@@ -23,48 +24,6 @@ func New() *UWeb {
 	return &UWeb{}
 }
 
-func (uw *UWeb) Get(path string, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func (uw *UWeb) Post(path string, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func (uw *UWeb) Put(path string, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func (uw *UWeb) Delete(path string, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func (uw *UWeb) Patch(path string, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func (uw *UWeb) Head(path string, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func (uw *UWeb) Options(path string, handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func (uw *UWeb) Run(port string) error {
-	return http.ListenAndServe(port, nil)
+func (uw *UWeb) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
 }
