c92dcc0947c81573666bf34dbc6a4886ff78cb73
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-20 07:32:36 -0300

    .

--- uweb.go
+++ uweb.go
@@ -31,14 +31,49 @@ func (uw *UWeb) Get(path string, fn http.HandlerFunc) {
 	uw.addNode(get, path, fn)
 }
 
+func (uw *UWeb) Post(path string, fn http.HandlerFunc) {
+	uw.addNode(post, path, fn)
+}
+
+func (uw *UWeb) Put(path string, fn http.HandlerFunc) {
+	uw.addNode(put, path, fn)
+}
+
+func (uw *UWeb) Patch(path string, fn http.HandlerFunc) {
+	uw.addNode(patch, path, fn)
+}
+
+func (uw *UWeb) Delete(path string, fn http.HandlerFunc) {
+	uw.addNode(delete, path, fn)
+}
+
+func (uw *UWeb) Options(path string, fn http.HandlerFunc) {
+	uw.addNode(options, path, fn)
+}
+
+func (uw *UWeb) Connect(path string, fn http.HandlerFunc) {
+	uw.addNode(connect, path, fn)
+}
+
+func (uw *UWeb) Trace(path string, fn http.HandlerFunc) {
+	uw.addNode(trace, path, fn)
+}
+
 func (uw *UWeb) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for _, v := range uw.tree {
-		if v.path == r.URL.Path {
-			v.handlerFn(w, r)
-		}
-	}
+	n := uw.findHandler(r)
+	n.handlerFn(w, r)
 }
 
 func (uw *UWeb) addNode(method, path string, handlerFn http.HandlerFunc) {
 	uw.tree = append(uw.tree, node{method: method, path: path, handlerFn: handlerFn})
+}
+
+func (uw *UWeb) findHandler(r *http.Request) node {
+	for _, v := range uw.tree {
+		if (v.path == r.URL.Path) && (v.method == r.Method) {
+			return v
+		}
+	}
+
+	return node{} // should be nil
 }
--- uweb_test.go
+++ uweb_test.go
@@ -1,1 +1,94 @@
 package uweb
+
+import (
+	"net/http"
+	"testing"
+)
+
+func BenchmarkGet(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
+
+func BenchmarkPost(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
+
+func BenchmarkPut(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Put("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
+
+func BenchmarkPatch(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Patch("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
+
+func BenchmarkDelete(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Delete("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
+
+func BenchmarkOptions(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Options("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
+
+func BenchmarkConnect(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Connect("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
+
+func BenchmarkTrace(b *testing.B) {
+	u := New()
+	msg := []byte("!")
+
+	for i := 0; i < b.N; i++ {
+		u.Connect("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write(msg)
+		})
+	}
+}
