f4996e339749c9fea525887c28bc859b9ff485c0
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-06-22 15:02:18 -0300

    .

--- path.go
+++ path.go
@@ -3,19 +3,19 @@ package uweb
 import "strings"
 
 func normalizePath(path string) string {
-	path = strings.Replace(strings.ToLower(path), " ", "", -1)
+	normalizedPath := strings.Replace(strings.ToLower(path), " ", "", -1)
 
-	if len(path) == 0 {
-		path = "/"
+	if len(normalizedPath) == 0 {
+		normalizedPath = "/"
 	}
 
-	if len(path) > 1 && path[len(path)-1:] == "/" {
-		path = path[:len(path)-1]
+	if len(normalizedPath) > 1 && normalizedPath[len(normalizedPath)-1:] == "/" {
+		normalizedPath = normalizedPath[:len(normalizedPath)-1]
 	}
 
-	if len(path) >= 1 && path[:1] != "/" {
-		path = "/" + path[:len(path)]
+	if normalizedPath[:1] != "/" {
+		normalizedPath = "/" + normalizedPath[:len(normalizedPath)]
 	}
 
-	return path
+	return normalizedPath
 }
